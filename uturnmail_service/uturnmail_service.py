623d2bf3b5441e516ad0c88944a062c189fab7d6
Sep Taheri <sept@uvic.ca>
2014-12-14 22:21:13 -0800

    added functions

--- uturnmail_service/uturnmail_service.py
+++ uturnmail_service/uturnmail_service.py
@@ -2,27 +2,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 def authenticate():
     #implement auth
     return "Authenticated"
 
+@app.route('/email_validator')
+def email_validator():
+    return 'email_validator!'
+
+
 @app.route('/create_uturn')
 def create_uturn():
+        #receives from RMQ
     return 'create_uturn !'
 
 @app.route('/create_adguru')
 def create_adguru():
+        #receives from RMQ
     return 'create_adguru!'
 
 @app.route('/sendmail_adguru')
 def sendmail_adguru():
+        #receives from RMQ
     return 'Hello World!'
 
+@app.route('/visit_url')
+def visit_url():
+    #receives from RMQ
+    return 'visit_url!'
 
 
 if __name__ == '__main__':
